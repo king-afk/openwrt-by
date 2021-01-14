861cd69adba07f915a466123a17a3f68b4143f51
king-afk <54397030+king-afk@users.noreply.github.com>
2021-01-14 23:27:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.0.0.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/kenzok8/openwrt-packages /package/openwrt-package
+git clone https://github.com/kenzok8/small /package/small
